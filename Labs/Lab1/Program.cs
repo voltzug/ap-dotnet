1dbd1d68008f2ce29c89e98dec09118524ae779a
Volodymyr <volodymyr.tsukanov.23@gmail.com>
2025-11-12 22:39:17 +0100

    Lab1

---
+++ Labs/Lab1/Program.cs
@@ -0,0 +1,54 @@
+﻿// notes
+// !!! use tryParse
+using Lab1;
+
+Console.WriteLine(UI.SIG_QUIT + " - dla przervania operacji");
+
+UI.PrintTaskTitle("1.3");
+{
+  const string BUZZ = "Buzz";
+
+  for (int i = 0; i <= 100; i++)
+  {
+    bool isMod3 = i % 3 == 0;
+    bool isMod5 = i % 5 == 0;
+
+    if (isMod3)
+    {
+      Console.Write("Fizz");
+      if (isMod5)
+      {
+        Console.WriteLine(BUZZ);
+      }
+      else
+      {
+        Console.WriteLine();
+      }
+    }
+    else if (isMod5) Console.WriteLine(BUZZ);
+    else Console.WriteLine(i);
+  }
+}
+
+UI.PrintTaskTitle("1.4-5");
+{
+  var rand = new Random();
+  int value = rand.Next(1, 101), guess = 0, tries = 0;
+  try
+  {
+    Console.Write("Sproboj zgadnonc: ");
+    while (guess != value && tries++ < 9)
+    {
+      UI.ReadIntLine(out guess);
+      if (value < guess) Console.Write("mniejsza");
+      else if (value > guess) Console.Write("wieksza");
+    }
+  }
+  catch (UIException)
+  {
+    Console.WriteLine("Force quit");
+  }
+  Console.WriteLine();
+  if (tries > 9) Console.WriteLine("Przegrales");
+  else Console.WriteLine("Zgadnoles z " + tries + " razu");
+}
---
+++ Labs/Lab1/UI.cs
@@ -0,0 +1,50 @@
+﻿namespace Lab1;
+
+public class UIException : Exception
+{
+  public UIException(string message)
+      : base(message)
+  {
+  }
+
+  public UIException(string message, Exception innerException)
+      : base(message, innerException)
+  {
+  }
+}
+
+public class UI
+{
+  public const string SIG_QUIT = "!";
+
+  static void CatchSIGs(string input)
+  {
+    if (input.StartsWith(SIG_QUIT))
+    {
+      throw new UIException("operation quit");
+    }
+  }
+
+  public static void PrintTaskTitle(string title)
+  {
+    Console.WriteLine("\n# " + title);
+  }
+
+  public static bool ReadIntLine(out int res, bool verbose = true)
+  {
+    res = 0;
+    string? input = Console.ReadLine();
+    if (input == null)
+    {
+      if (verbose) Console.WriteLine("Nie wprowadzono liczby");
+      return false;
+    }
+    CatchSIGs(input);
+    if (int.TryParse(input, out res) == false)
+    {
+      if (verbose) Console.WriteLine("To nie jest liczba calkowita");
+      return false;
+    }
+    return true;
+  }
+}
